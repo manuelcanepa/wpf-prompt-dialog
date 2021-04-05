95a9f68a97d8b31ef40d42415a5ff5ba741cfb8f
Manuel Cánepa <manuelcanepa@gmail.com>
2021-04-05 00:14:02 -0300

    First release
    See https://stackoverflow.com/questions/2796470/wpf-create-a-dialog-prompt/17811611#17811611

---
+++ PromptDialog/Dialog.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows;
+
+namespace PromptDialog
+{
+    /// <summary>
+    /// Interaction logic for PromptDialog.xaml
+    /// </summary>
+    public partial class Dialog : Window
+    {
+        public enum InputType
+        {
+            Fecha,
+            Text,
+            Password,
+            Numero
+        }
+
+        private InputType _inputType = InputType.Text;
+
+        private string _defaultValue;
+
+        public Dialog(string question, string title, string defaultValue = "", InputType inputType = InputType.Text)
+        {
+            InitializeComponent();
+            Loaded += new RoutedEventHandler(PromptDialog_Loaded);
+            txtQuestion.Text = question;
+            Title = title;
+            _defaultValue = defaultValue;
+            _inputType = inputType;
+        }
+
+        private void PromptDialog_Loaded(object sender, RoutedEventArgs e)
+        {
+            switch (_inputType)
+            {
+                case InputType.Password:
+                    txtPasswordResponse.Visibility = Visibility.Visible;
+                    txtPasswordResponse.Focus();
+                    break;
+                case InputType.Fecha:
+                    dtpDateResponse.Visibility = Visibility.Visible;
+                    if (!string.IsNullOrEmpty(_defaultValue))
+                        dtpDateResponse.SelectedDate = DateTime.Parse(_defaultValue);
+
+                    dtpDateResponse.Focus();
+                    break;
+                default:
+                    txtResponse.Visibility = Visibility.Visible;
+                    txtResponse.Text = _defaultValue;
+                    txtResponse.Focus();
+                    txtResponse.SelectAll();
+                    break;
+            }
+        }
+
+        public static object Prompt(string question, string title, string defaultValue = "", InputType inputType = InputType.Text)
+        {
+            Dialog inst = new Dialog(question, title, defaultValue, inputType);
+            inst.ShowDialog();
+            if (inst.DialogResult == true)
+                return inst.ResponseText;
+            return null;
+        }
+
+        public object ResponseText
+        {
+            get
+            {
+                switch (_inputType)
+                {
+                    case InputType.Password: return txtPasswordResponse.Password;
+                    case InputType.Fecha: return dtpDateResponse.SelectedDate;
+                    default: return txtResponse.Text;
+                }
+            }
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ PromptDialog/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PromptDialog")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ManuelCanepa")]
+[assembly: AssemblyProduct("PromptDialog")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("704ed8f8-9020-4d57-a9cd-67ff2d2dba1e")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
