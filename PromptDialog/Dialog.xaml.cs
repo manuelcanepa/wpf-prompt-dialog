491dbc57ed2e47024daf5c15b0d09188e5880c40
Martin Braun <martin.braun.1991@live.com>
2022-01-22 09:24:32 +0100

    Rename Fecha and Numero to Date and Number
    Fixes #1

--- PromptDialog/Dialog.xaml.cs
+++ PromptDialog/Dialog.xaml.cs
@@ -10,10 +10,10 @@ namespace PromptDialog
     {
         public enum InputType
         {
-            Fecha,
+            Date,
             Text,
             Password,
-            Numero
+            Number
         }
 
         private InputType _inputType = InputType.Text;
@@ -38,7 +38,7 @@ namespace PromptDialog
                     txtPasswordResponse.Visibility = Visibility.Visible;
                     txtPasswordResponse.Focus();
                     break;
-                case InputType.Fecha:
+                case InputType.Date:
                     dtpDateResponse.Visibility = Visibility.Visible;
                     if (!string.IsNullOrEmpty(_defaultValue))
                         dtpDateResponse.SelectedDate = DateTime.Parse(_defaultValue);
